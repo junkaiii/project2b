cafa88c8fa0d29e91417f951452a9f7d363b4108
Jun Kai Lim <junkaiii@gmail.com>
2016-07-27 22:21:11 +0800

    added email and password validation, added password confirmation, added password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+before_save { email.downcase! }
+
+# For hasing password using bcrypt
+has_secure_password
+
+#VALIDATIONS
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+validates :email,
+          presence: true,
+          length: { maximum: 255, message: 'You sure?'},
+          format: { with: VALID_EMAIL_REGEX },
+          uniqueness: true
+
+
+validates :password,
+          length: { in: 6..20 }
+
+
 end
