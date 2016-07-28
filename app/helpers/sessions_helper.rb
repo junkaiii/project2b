0abeb92007dcfd5e4fe5ec14aca74d97c741236d
Jun Kai Lim <junkaiii@gmail.com>
2016-07-28 13:17:07 +0800

    full logout compatibility

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,20 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Get current user from session id
+  def current_user
+  @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # Check if current user is nil
+  def logged_in?
+    current_user
+  end
+
   #Logs out current user
   def log_out
     session.delete(:user_id)
     @current_user = nil
   end
-  
+
 end
