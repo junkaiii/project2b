14e87af79b044cf74705b390cf00d05c45223f9b
Jun Kai Lim <junkaiii@gmail.com>
2016-07-29 18:12:11 +0800

    still working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :jobs, dependent: destroy #if user is deleted, all related job is deleted
   attr_accessor :remember_token
   before_save { email.downcase! }
 
